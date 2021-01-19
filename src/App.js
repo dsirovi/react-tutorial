a34314462c468b115858cf9dfcd28d16225b0ec4
dsirovi <domagoj.sirovina@hotmail.com>
2021-01-19 11:35:36 +0100

    First assignment: create one dynamic user input and two users output with using state

--- src/App.js
+++ src/App.js
@@ -1,70 +1,32 @@
-import React, {useState} from "react";
+import React, {useState, Component} from "react";
 import './App.css';
 import Person from './Person/Person';
 
-const app = props => {
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const [personsState, setPersonsState] = useState({
-        persons: [
-            {name: 'Max', age: 28},
-            {name: 'Manu', age: 22},
-            {name: 'Michele', age: 21}
-        ],
-        otherValue: 'Some value'
-    });
+import UserInput from "./UserInput/UserInput";
+import UserOutput from "./UserOutput/UserOutput";
 
-    console.log(personsState);
-
-    const switchNameHandler = (newName) => {
-        console.log('Was clicked!');
-        // DO NOT DO THIS: personsState.persons[0].name = 'Maximilian';
-        setPersonsState({
-            persons: [
-                {name: newName, age: 28},
-                {name: 'Manu', age: 22},
-                {name: 'Michele', age: 33}
-            ]
-        })
-    };
-
-    const nameChangeHandler = (event) => {
-        // eslint-disable-next-line react-hooks/rules-of-hooks
-        setPersonsState({
-            persons: [
-                {name: 'Max', age: 28},
-                {name: event.target.value, age: 22},
-                {name: 'Michele', age: 21}
-            ]
-        })
+class App extends Component {
+    state = {
+        username: 'Super Max'
     }
 
-    const buttonStyle = {
-        background: 'white',
-        font: 'inherit',
-        border: '1px solid black',
-        padding: '8px',
-        cursor: 'pointer'
-    };
+    usernameChangeHandler = (event) => {
+        this.setState({username: event.target.value})
+    }
 
-    return (
-        <div className="App">
-            <h1>Hi, I'm React App</h1>
-            {/*Do not use to often*/}
-            <button style={buttonStyle}
-                    onClick={() => switchNameHandler('Willllly')}>Switch Name
-            </button>
-            <Person name={personsState.persons[0].name}
-                    age={personsState.persons[0].age}/>
-            <Person click={switchNameHandler.bind(this, 'Trash')}
-                    name={personsState.persons[1].name}
-                    age={personsState.persons[1].age}
-                    changed={nameChangeHandler}
-            >My hobbies: Racing</Person>
-            <Person name={personsState.persons[2].name}
-                    age={personsState.persons[2].age}/>
-        </div>
-    );
+    render() {
+        return (
+            <div className="App">
+                <UserInput changed={this.usernameChangeHandler} currentName={this.state.username}/>
+                <UserOutput userName={this.state.username}/>
+                <UserOutput userName={this.state.username}/>
+                <UserOutput userName="Max"/>
+            </div>
+        );
+    }
+
+
 }
 
 
-export default app;
+export default App;
---
+++ src/UserInput/UserInput.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const userInput = (props) => {
+    const style = {
+        border: '2px solid red'
+    }
+    return (
+        <div>
+            <input type="text" onChange={props.changed} value={props.currentName} style={style}/>
+        </div>
+    )
+}
+
+export default userInput;
